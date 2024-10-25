1e2364a7f25ef9be56302fe1849ad8ec6dc0ab1c
Belissimo_T <bela.tollking@gmail.com>
2024-10-26 00:25:20 +0200

    fix: add contextlib.suppress(OSError)

--- my_cache.py
+++ my_cache.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import concurrent.futures
+import contextlib
 import dataclasses
 import datetime
 import io
@@ -10,7 +11,6 @@ import multiprocessing
 import os
 import random
 import shutil
-import sys
 import tarfile
 import time
 import typing
@@ -134,8 +134,10 @@ class FileSystemStore:
 
     def save_file(self, filename: str, content: bytes):
         self.path.mkdir(parents=True, exist_ok=True)
-        (self.path / (filename + ".tmp")).write_bytes(content)
-        (self.path / (filename + ".tmp")).rename(self.path / filename)
+
+        with contextlib.suppress(OSError):
+            (self.path / (filename + ".tmp")).write_bytes(content)
+            (self.path / (filename + ".tmp")).rename(self.path / filename)
 
     def read_file(self, filename: str) -> bytes:
         return (self.path / filename).read_bytes()
