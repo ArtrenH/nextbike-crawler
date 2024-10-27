f59d7b886c0c093f9198b1e02f8d261d011199ad
Belissimo_T <bela.tollking@gmail.com>
2024-10-27 02:31:24 +0100

    feat: add field to .env file

--- crawler.py
+++ crawler.py
@@ -25,13 +25,16 @@ class NextBikeCrawler:
     wait_interval: int = 10
 
     def update(self, crawl_time: datetime.datetime, session: requests.Session):
-        log.info(f"Updating data at {crawl_time}")
+        log.info(f"Fetching data for {crawl_time}")
+        t1 = time.perf_counter()
         response = session.get('https://maps.nextbike.net/maps/nextbike-live.json', headers={'Accept-Encoding': 'gzip'})
+        log.info(f"fetched ({time.perf_counter() - t1:.2f} s)")
         self.cache.save_file(crawl_time, response.json())
-        log.info("...done")
+        log.info(f"saved {crawl_time}")
 
     def wait_until_next_update(self) -> datetime.datetime:
         next_time = (time.time() // self.wait_interval + 1) * self.wait_interval
+        log.debug(f"waiting {next_time - time.time():.1f} s")
         time.sleep(next_time - time.time())
         return datetime.datetime.fromtimestamp(next_time, datetime.timezone.utc)
 
@@ -46,22 +49,25 @@ class NextBikeCrawler:
 
 def main():
     logging.basicConfig(level=logging.DEBUG, format="[%(asctime)s] [%(levelname)8s] %(name)s: %(message)s",
-                        datefmt="%Y-%m-%d %H:%M:%S")
+                        datefmt="%Y-%m-%d %H:%M:%S", force=True)
     logging.getLogger("urllib3").setLevel(logging.CRITICAL)
     logging.getLogger("urllib3").propagate = False
     logging.getLogger("charset_normalizer").setLevel(logging.CRITICAL)
     logging.getLogger("charset_normalizer").propagate = False
 
-    with concurrent.futures.ThreadPoolExecutor(5) as executor:
+    with concurrent.futures.ProcessPoolExecutor(max_workers=int(os.getenv("MAX_WORKERS", None))) as executor:
         cache = Cache(
-            file_path=Path("cache"),
+            file_path=Path(os.getenv("CACHE_PATH", "cache")),
             executor=executor,
             data_store=FileSystemStore(Path(os.getenv("DATA_STORE_PATH", "cache/data"))),
-            base_file_creation_interval=30*60
+            base_file_creation_interval=int(os.getenv("BASE_FILE_CREATION_INTERVAL", 30 * 60)),
         )
         cache.init()
 
-        crawler = NextBikeCrawler(cache)
+        crawler = NextBikeCrawler(
+            cache=cache,
+            wait_interval=int(os.getenv("WAIT_INTERVAL", 10))
+        )
 
         crawler.crawl()
 
