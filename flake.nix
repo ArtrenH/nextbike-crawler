4cf86f4e7963b9999ca06d2265d81108a1233333
Pipifax <37810842+Belissimo-T@users.noreply.github.com>
2025-12-26 22:04:15 +0100

    fix nix things

--- flake.nix
+++ flake.nix
@@ -24,18 +24,26 @@
           pkgs.darwin.apple_sdk.frameworks.CoreFoundation
         ];
 
+        __pythonPkg = (pkgs.python312.withPackages (ps: with ps; [
+#          pip
+          virtualenv
+#          setuptools
+#          wheel
+#          cython
+          tkinter
+        ]));
+
         # 2. Common Tools
         devTools = with pkgs; [
           pkg-config
-          python312
-          python312Packages.pip
-          python312Packages.virtualenv
+          __pythonPkg
           cargo
           rustc
           rust-analyzer
           maturin
           gcc
-          zsh  # <--- Added ZSH here
+          tk
+          tcl
         ];
 
         # 3. Common Shell Hook
@@ -49,25 +57,34 @@
         devShells = {
           # ---------------------------------------------------------
           # Shell 1: Default (Auto-setup)
-          # usage: 'nix develop'
+          # usage: 'nix develop -c $SHELL'
           # ---------------------------------------------------------
           default = pkgs.mkShell {
             nativeBuildInputs = devTools;
             buildInputs = buildLibraries;
 
             shellHook = baseShellHook + ''
+              export VENV_DIR="$PWD/.venv"
+
               # 1. Auto-setup logic
-              if [ ! -d ".venv" ]; then
+              if [ ! -d "$VENV_DIR" ]; then
                 echo "Creating virtual environment..."
-                python -m venv .venv
+                ${__pythonPkg}/bin/python -m venv $VENV_DIR
               fi
 
               # 2. Activate venv (Sets PATH and VIRTUAL_ENV)
-              source .venv/bin/activate
+              source $VENV_DIR/bin/activate
+
+              # Set up paths
+              export PYTHONPATH="${__pythonPkg}/lib/${__pythonPkg.libPrefix}/site-packages:$PYTHONPATH"
+              export TCL_LIBRARY="${pkgs.tcl}/lib/tcl${pkgs.lib.versions.majorMinor pkgs.tcl.version}"
+              export TK_LIBRARY="${pkgs.tk}/lib/tk${pkgs.lib.versions.majorMinor pkgs.tk.version}"
+
+              pip install --upgrade pip
 
               # 3. Install requirements
               if [ -f "requirements.txt" ]; then
-                echo "Syncing requirements..."
+                echo "Installing requirements..."
                 pip install -r requirements.txt
               fi
             '';
@@ -82,7 +99,14 @@
             buildInputs = buildLibraries;
 
             shellHook = baseShellHook + ''
-              source .venv/bin/activate
+              export VENV_DIR="$PWD/.venv"
+
+              source $VENV_DIR/bin/activate
+
+              # Set up paths
+              export PYTHONPATH="${__pythonPkg}/lib/${__pythonPkg.libPrefix}/site-packages:$PYTHONPATH"
+              export TCL_LIBRARY="${pkgs.tcl}/lib/tcl${pkgs.lib.versions.majorMinor pkgs.tcl.version}"
+              export TK_LIBRARY="${pkgs.tk}/lib/tk${pkgs.lib.versions.majorMinor pkgs.tk.version}"
             '';
           };
         };
--- nix/nix-python.sh
+++ nix/nix-python.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SCRIPT_PATH=$(dirname "$0")
 FLAKE_PATH=$(realpath "$SCRIPT_PATH/..")
-exec nix develop "$FLAKE_PATH#python" --command python "$@"
+exec nix develop "$FLAKE_PATH#python" --command "$FLAKE_PATH/.venv/bin/python" "$@"
